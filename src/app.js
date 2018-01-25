c35d832278953ee02ef79e7edc145cdcb4e374e7
Denis Juan Alonso Glez <denjalonso@gmail.com>
2018-01-25 18:28:52 +0000

    Desde datos a valores de salida, trabajando con scalas.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-document.body.innerHTML = `<h1>Hola desde ${d3.version}</h1>`;
+// make console.log write to the page for better in-browser experience
+(function () {
+    var body = document.querySelector('body');
+    body.style['fontFamily'] = 'monospace';
+    body.style['fontSize'] = '2em';
+    console.log = function (x) { body.innerText += x + '\n'; };
+}());
+
+var linearScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([0, 1])
+    .clamp(true);
+
+console.log(linearScale(0));
+console.log(linearScale(50));
+console.log(linearScale(100));
+
+console.log(linearScale.invert(300));
